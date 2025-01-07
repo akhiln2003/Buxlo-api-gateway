0836a79073675e928c5c268487cc68f9dc7ce1da
akhil <4khiln@gmail.com>
2025-01-07 17:04:36 +0530

    feat: Update proxy routes for admin and user services
    - Added new routes for admin profile endpoints:
      - /api/auth/admin/profile/fetchusers
      - /api/auth/admin/profile/fetchmentor
    - Introduced /api/user route with auth enabled, pointing to USER_SERVICE_URL.
    - Ensured proper proxy configuration with changeOrigin set to true.
    - Improved route structure to handle both admin and user requests efficiently.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,22 @@ require('dotenv').config();
 
 export const ROUTES = [
     {
+        url: '/api/auth/admin/profile/fetchusers',
+        auth: true,
+        proxy: {
+            target: `${process.env.AUTH_SERVICE_URL}/auth/admin/profile/fetchusers`,
+            changeOrigin: true,
+        }
+    },
+    {
+        url: '/api/auth/admin/profile/fetchmentor',
+        auth: true,
+        proxy: {
+            target: `${process.env.AUTH_SERVICE_URL}/auth/admin/profile/fetchmentor`,
+            changeOrigin: true,
+        }
+    },
+    {
         url: '/api/auth',
         auth: false,
         proxy: {
@@ -9,8 +25,13 @@ export const ROUTES = [
             changeOrigin: true,
         }
     },
+    {
+        url: '/api/user',
+        auth:true,
+        proxy:{
+            target: `${process.env.USER_SERVICE_URL}/user`,
+            changeOrigin: true,
+        }
+    }
     
-    
-    
-   
 ]
